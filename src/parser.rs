bf03f3c8a6a9a22829996f6aecd7ad17c5db26c4
José Duarte <jmg.duarte@campus.fct.unl.pt>
2020-12-24 18:42:11 +0000

    Add documentation for `RequestLineParser` type alias

--- src/parser.rs
+++ src/parser.rs
@@ -16,6 +16,7 @@ const TAB: u8 = '\t' as u8;
 /// Possible transitions which can error return `Result<T, ParsingError>`.
 type Result<T> = std::result::Result<T, ParsingError>;
 
+/// Short form for `HttpRequestParser<'a, RequestLine<S>>`.
 type RequestLineParser<'a, S> = HttpRequestParser<'a, RequestLine<S>>;
 
 // TODO add more traits
